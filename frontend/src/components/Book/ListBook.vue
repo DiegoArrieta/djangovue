d1fb3a24b4bf890ea7d8406988b0865d2fd114b6
Diego Arrieta <darrieta.lnx@gmail.com>
2019-03-30 15:50:39 -0300

    inicio

---
+++ frontend/src/components/Book/ListBook.vue
@@ -0,0 +1,44 @@
+<template lang="html">
+  <div class="container">
+    <div class="row">
+      <div class="col text-left">
+        <h2>Listado de Libros</h2>
+        <div class="col-md-12">
+          <b-table striped hover :items="books" :fields="fields">
+
+          </b-table>
+        </div>
+      </div>
+    </div>
+  </div>
+  </template>
+<script>
+import axios from 'axios';
+export default {
+    data (){
+      return {
+        fields: [
+          { key: 'title', label: 'Titulo'},
+          { key: 'description', label: 'Descripcion'},
+          { key: 'action', label: 'Acción'}
+        ],
+        books: []
+      }
+    },
+    methods: {
+      getBooks(){
+        const path = 'http://localhost:8000/api/v1.0/books/'
+        axios.get(path).then((response)=> {
+          this.books = response.data
+        })
+          .catch ((error)=>{
+          console.log(error)
+        })
+      }
+    },
+    created(){
+      this.getBooks()
+    }
+  }
+</script>
+<style lang="css" scoped></style>
---
+++ frontend/src/router/index.js
@@ -0,0 +1,23 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import HelloWorld from '@/components/HelloWorld'
+import ListBook from '@/components/Book/ListBook'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'HelloWorld',
+      component: HelloWorld
+    },
+    {
+      path: '/books',
+      name: 'ListBook',
+      component: ListBook
+    },
+  ]
+  ,
+  mode: 'history'
+})
